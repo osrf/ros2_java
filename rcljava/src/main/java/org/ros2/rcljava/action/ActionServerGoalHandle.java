4ced1ad39c5cb8cb8bbaa9495916433b48efb819
Jacob Perron <jacob@openrobotics.org>
2020-10-20 00:05:34 +0000

    [wip] Finish action server implementation and unit tests
    Signed-off-by: Jacob Perron <jacob@openrobotics.org>

--- rcljava/src/main/java/org/ros2/rcljava/action/ActionServerGoalHandle.java
+++ rcljava/src/main/java/org/ros2/rcljava/action/ActionServerGoalHandle.java
@@ -15,8 +15,77 @@
 
 package org.ros2.rcljava.action;
 
-import org.ros2.rcljava.interfaces.ActionDefinition;
+import java.util.List;
 
-public class ActionServerGoalHandle<T extends ActionDefinition> {
+import org.ros2.rcljava.common.JNIUtils;
+import org.ros2.rcljava.interfaces.ActionDefinition;
+import org.ros2.rcljava.interfaces.Disposable;
+import org.ros2.rcljava.interfaces.MessageDefinition;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ActionServerGoalHandle<T extends ActionDefinition>  implements Disposable {
+  private static final Logger logger = LoggerFactory.getLogger(ActionServerGoalHandle.class);
+
+  static {
+    try {
+      JNIUtils.loadImplementation(ActionServerGoalHandle.class);
+    } catch (UnsatisfiedLinkError ule) {
+      logger.error("Native code library failed to load.\n" + ule);
+      System.exit(1);
+    }
+  }
+
+  private long handle;
+  private MessageDefinition goal;
+
+  private static native long nativeAcceptNewGoal(
+    long actionServerHandle,
+    long goalInfoFromJavaConverterHandle,
+    long goalInfoDestructorHandle,
+    MessageDefinition goalInfo);
+
+  private static native void nativeDispose(long handle);
+
+  public ActionServerGoalHandle(
+    ActionServer<T> actionServer, List<Byte> uuid, MessageDefinition goal)
+  {
+    action_msgs.msg.GoalInfo goalInfo = new action_msgs.msg.GoalInfo();
+    unique_identifier_msgs.msg.UUID uuidMessage= new unique_identifier_msgs.msg.UUID();
+    uuidMessage.setUuid(uuid);
+    goalInfo.setGoalId(uuidMessage);
+    // TODO: set time
+    // goalInfo.setStamp(this.timeAccepted);
+
+    long goalInfoFromJavaConverterHandle = goalInfo.getFromJavaConverterInstance();
+    long goalInfoDestructorHandle = goalInfo.getDestructorInstance();
+
+    this.handle = nativeAcceptNewGoal(
+      actionServer.getHandle(),
+      goalInfoFromJavaConverterHandle,
+      goalInfoDestructorHandle,
+      goalInfo);
+    this.goal = goal;
+  }
+
+  public MessageDefinition getGoal() {
+    return this.goal;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public final void dispose() {
+    nativeDispose(this.handle);
+    this.handle = 0;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public final long getHandle() {
+    return handle;
+  }
 
 }
